b830781990b1b513a6d4e90583b4668d9b707db5
George Kelly <george.kelly@ukfast.co.uk>
2020-08-21 09:59:33 +0100

    Removed setup(): void causing 5.6 runner to fail

--- tests/Middleware/BasicAuthTest.php
+++ tests/Middleware/BasicAuthTest.php
@@ -8,21 +8,16 @@ use UKFast\HealthCheck\Middleware\BasicAuth;
 
 class BasicAuthTest extends TestCase
 {
-    public function setUp(): void
-    {
-        parent::setUp();
-
-        config([
-            'healthcheck.auth.user' => 'correct-user',
-            'healthcheck.auth.password' => 'correct-password'
-        ]);
-    }
-
     /**
      * @test
      */
     public function only_shows_status_code_if_fails_basic_auth()
     {
+        config([
+            'healthcheck.auth.user' => 'correct-user',
+            'healthcheck.auth.password' => 'correct-password'
+        ]);
+
         $request = Request::create('/health', 'GET', [], [], [], [
             'PHP_AUTH_USER' => 'wrong-user',
             'PHP_AUTH_PW' => 'wrong-password',
@@ -41,6 +36,11 @@ class BasicAuthTest extends TestCase
      */
     public function only_shows_status_code_if_no_auth_credentials_are_passed()
     {
+        config([
+            'healthcheck.auth.user' => 'correct-user',
+            'healthcheck.auth.password' => 'correct-password'
+        ]);
+
         $request = Request::create('/health', 'GET');
 
         $response = (new BasicAuth)->handle($request, function () {
@@ -56,6 +56,11 @@ class BasicAuthTest extends TestCase
      */
     public function shows_full_response_if_passes_basic_auth()
     {
+        config([
+            'healthcheck.auth.user' => 'correct-user',
+            'healthcheck.auth.password' => 'correct-password'
+        ]);
+
         $request = Request::create('/health', 'GET', [], [], [], [
             'PHP_AUTH_USER' => 'correct-user',
             'PHP_AUTH_PW' => 'correct-password',
