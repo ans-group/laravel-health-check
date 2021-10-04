dd633119c5e8ad3a41c0ea47cf0211ee12babe5c
Adam Kirk <adam_kirk@live.co.uk>
2021-10-04 09:24:20 +0100

    Add compatability for redis clusters in redis check
    The RedisHealthCheck faield when using a redis cluster and phpredis
    due to the requirement of an argument to the ping method on the
    RedisCluster class (found in the redis extension).
    
    This change adds compatability by detecting which redis connection
    we are using, and then handling the ping appropriately for either
    a cluster or a single node.
    
    The tests for RedisHealthcheck have been reworked to ensure that
    the correct flow is taken dependent on configuration and connection
    type being used.

--- src/Checks/RedisHealthCheck.php
+++ src/Checks/RedisHealthCheck.php
@@ -2,6 +2,7 @@
 
 namespace UKFast\HealthCheck\Checks;
 
+use Illuminate\Redis\Connections\PhpRedisClusterConnection;
 use Illuminate\Support\Facades\Redis;
 use UKFast\HealthCheck\HealthCheck;
 use Exception;
@@ -13,12 +14,38 @@ class RedisHealthCheck extends HealthCheck
     public function status()
     {
         try {
-            Redis::ping();
+            if ($this->isUsingPhpRedis()) {
+                $this->handlePhpRedisPing();
+            } else {
+                // Think this is all we can do for predis
+                Redis::ping();
+            }
         } catch (Exception $e) {
             return $this->problem('Failed to connect to redis', [
                 'exception' => $this->exceptionContext($e),
             ]);
         }
         return $this->okay();
+    }
+
+    protected function isUsingPhpRedis()
+    {
+        return config('database.redis.client') == 'phpredis';
+    }
+
+    protected function handlePhpRedisPing()
+    {
+        $redis = Redis::connection();
+
+        if (! $redis instanceof PhpRedisClusterConnection) {
+            $redis->ping();
+            
+            return;
+        }
+
+        
+        foreach ($redis->_masters() as $master) {
+            $redis->ping($master);
+        }
     }
 }
